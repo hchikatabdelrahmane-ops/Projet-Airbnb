de908e2fd0abe768d0672130aa6363ab27d255e6
ismail <ismailyous@yahoo.com>
2025-11-21 13:13:38 +0100

    tp final

---
+++ Projet-Airbnb/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbh   = 'logement';   
+$user = 'root';       
+$pass = '';           
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbh;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+?>
