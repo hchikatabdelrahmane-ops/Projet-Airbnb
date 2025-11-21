213837e6b3987af375f8733099ed69dd340a954a
hchikatabdelrahmane-ops <hchikatabdelrahmane@gmail.com>
2025-11-21 13:48:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "airbnb";
+$database = "airbnb";   //base = logement pour abdelrahmane
 
 $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
